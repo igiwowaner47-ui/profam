ffb5e140771cf9a25b97f21af8e2e7b55f1fc97e
alex-hh <alex.hhooker@gmail.com>
2024-06-23 20:48:15 +0100

    add callbacks

---
+++ src/utils/callbacks.py
@@ -0,0 +1,8 @@
+from lightning.pytorch.callbacks import Callback
+
+
+class ShuffleCallback(Callback):
+    def on_train_epoch_start(self, trainer, pl_module):
+        print("Shuffling")
+        # https://huggingface.co/docs/datasets/v2.20.0/en/package_reference/main_classes#datasets.Dataset.to_iterable_dataset
+        trainer.train_dataloader.dataset.set_epoch(trainer.current_epoch)
