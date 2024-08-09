a11e2ee1ba943ca2a3c918029e79e7b14ac318b0
alex-hh <alex.hhooker@gmail.com>
2024-08-09 01:02:18 +0100

    adjust accumulate grad batches in distributed training setting

--- src/utils/trainer.py
+++ src/utils/trainer.py
@@ -1,5 +1,5 @@
 import math
-
+import torch
 from lightning import Trainer
 
 
@@ -13,11 +13,16 @@ class ProFamTrainer(Trainer):
         **kwargs
     ):
         if target_tokens_per_batch is not None:
+            devices = kwargs.get("devices", "auto")
+            if devices == "auto":
+                assert torch.cuda.is_available()
+                devices = torch.cuda.device_count()
+
             assert (
                 "accumulate_grad_batches" not in kwargs
             ), "accumulate_grad_batches should not be set when target_tokens_per_batch is set"
             kwargs["accumulate_grad_batches"] = math.ceil(
-                target_tokens_per_batch / (tokens_per_document * batch_size)
+                target_tokens_per_batch / (tokens_per_document * batch_size * devices)
             )
             print(
                 "Setting accumulate_grad_batches to", kwargs["accumulate_grad_batches"]
