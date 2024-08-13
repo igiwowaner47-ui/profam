2b71efff62e160d400338e20c21045b5d45e8453
alex-hh <alex.hhooker@gmail.com>
2024-08-13 19:29:23 +0100

    max seq pos -1

--- src/data/utils.py
+++ src/data/utils.py
@@ -87,9 +87,9 @@ def get_flat_seq_pos_from_positions(
         flat_positions = [prepend_index] * num_start_tokens
         for sequence_positions in positions[:-1]:
             # add 1 so that sep doesnt have same position index
-            flat_positions += [min(p + 1, max_seq_pos) for p in sequence_positions]
+            flat_positions += [min(p + 1, max_seq_pos-1) for p in sequence_positions]
             flat_positions.append(sep_index)
-        flat_positions += [min(p + 1, max_seq_pos) for p in positions[-1]]
+        flat_positions += [min(p + 1, max_seq_pos-1) for p in positions[-1]]
         flat_positions += [append_index] * num_end_tokens
         return flat_positions
     else:
