e7e361f4a34eb0b4fcd72e0b2b029fc8675ab081
alex-hh <alex.hhooker@gmail.com>
2024-08-14 12:34:43 +0100

    add backbone coords from example helper fn

--- src/data/utils.py
+++ src/data/utils.py
@@ -380,3 +380,25 @@ def sample_to_max_tokens(
         sampled_sequences.append(seq)
         token_count += len(seq) + 1
     return sampled_sequences
+
+
+def backbone_coords_from_example(example):
+    ns = example["N"]
+    cas = example["CA"]
+    cs = example["C"]
+    oxys = example["O"]
+    coords = []
+    for seq, n, ca, c, o in zip(
+        example["sequences"],
+        ns,
+        cas,
+        cs,
+        oxys,
+    ):
+        recons_coords = np.zeros((len(seq), 4, 3))
+        recons_coords[:, 0] = n.reshape(-1, 3)
+        recons_coords[:, 1] = ca.reshape(-1, 3)
+        recons_coords[:, 2] = c.reshape(-1, 3)
+        recons_coords[:, 3] = o.reshape(-1, 3)
+        coords.append(recons_coords)
+    return coords
