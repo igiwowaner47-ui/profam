5ca94a7a6c5e878e571d0fe1d455e71c15390723
alex-hh <alex.hhooker@gmail.com>
2024-08-22 12:03:38 +0100

    fix tokenizer tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,20 +10,6 @@ import rootutils
 from hydra import compose, initialize
 from hydra.core.global_hydra import GlobalHydra
 from omegaconf import DictConfig, open_dict
-from transformers import PreTrainedTokenizerFast
-
-
-@pytest.fixture()
-def profam_tokenizer():
-    tokenizer = PreTrainedTokenizerFast(
-        tokenizer_file="src/data/components/profam_tokenizer.json",
-        unk_token="[UNK]",
-        pad_token="[PAD]",
-        bos_token="[start-of-document]",
-        sep_token="[SEP]",
-        mask_token="[MASK]",
-    )
-    return tokenizer
 
 from src.constants import BASEDIR
 from src.data.proteingym import load_gym_dataset
--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -2,8 +2,7 @@ from src.data.fasta import read_fasta_sequences
 from src.data.utils import sample_to_max_tokens
 
 
-
-def test_encode_decode(profam_tokenizer, pfam_fasta_text):
+def test_encode_decode(profam_tokenizer_seqpos, pfam_fasta_text):
     lines = pfam_fasta_text.split("\n")
     sequence_iterator = read_fasta_sequences(
         lines,
@@ -13,23 +12,26 @@ def test_encode_decode(profam_tokenizer, pfam_fasta_text):
         to_upper=True,
     )
     sequences = sample_to_max_tokens(
-        list(sequence_iterator), max_tokens=profam_tokenizer.max_tokens
+        list(sequence_iterator), max_tokens=profam_tokenizer_seqpos.max_tokens
     )
     # n.b. encode_sequences encodes as a sequence of sequences
-    encoded = profam_tokenizer.encode_sequences(sequences).input_ids
-    decoded = profam_tokenizer.decode_tokens(encoded.unsqueeze(0))[
+    encoded = profam_tokenizer_seqpos.encode_sequences(sequences).input_ids
+    decoded = profam_tokenizer_seqpos.decode_tokens(encoded.unsqueeze(0))[
         0
     ]  # decode_tokens returns a list of lists
     for input_seq, decoded_seq in zip(sequences, decoded):
         assert input_seq == decoded_seq
 
 
-def test_sequence_of_sequence_tokenization(profam_tokenizer):
+def test_sequence_of_sequence_tokenization(profam_tokenizer_seqpos):
     example_sequences = ["ARNDC", "QEGHIL", "KMFPST", "WYV"]
     concatenated_sequence = (
-        "[RAW]" + profam_tokenizer.bos_token + "[SEP]".join(example_sequences) + "[SEP]"
+        "[RAW]"
+        + profam_tokenizer_seqpos.bos_token
+        + "[SEP]".join(example_sequences)
+        + "[SEP]"
     )
-    tokenized = profam_tokenizer(
+    tokenized = profam_tokenizer_seqpos(
         concatenated_sequence,
         return_tensors="pt",
         truncation=False,
@@ -38,7 +40,9 @@ def test_sequence_of_sequence_tokenization(profam_tokenizer):
         add_special_tokens=False,
     )
     # TODO: extend...
-    assert tokenized.input_ids[0, 0] == profam_tokenizer.convert_tokens_to_ids("[RAW]")
+    assert tokenized.input_ids[0, 0] == profam_tokenizer_seqpos.convert_tokens_to_ids(
+        "[RAW]"
+    )
     assert not (
-        tokenized["input_ids"] == profam_tokenizer.convert_tokens_to_ids("[UNK]")
+        tokenized["input_ids"] == profam_tokenizer_seqpos.convert_tokens_to_ids("[UNK]")
     ).any()
