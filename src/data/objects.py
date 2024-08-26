1d3a2228e59088e181bfa7824832426c07378185
alex-hh <alex.hhooker@gmail.com>
2024-08-26 22:16:04 +0100

    support coords masks

--- src/data/objects.py
+++ src/data/objects.py
@@ -25,6 +25,7 @@ class Protein:
     positions: Optional[List[int]] = None
     plddt: Optional[np.ndarray] = None
     backbone_coords: Optional[np.ndarray] = None
+    backbone_coords_mask: Optional[np.ndarray] = None
     structure_tokens: Optional[str] = None
 
 
@@ -53,6 +54,7 @@ class ProteinDocument:
     positions: Optional[List[List[int]]] = None
     plddts: Optional[List[np.ndarray]] = None
     backbone_coords: Optional[List[np.ndarray]] = None
+    backbone_coords_masks: Optional[List[np.ndarray]] = None
     structure_tokens: Optional[List[str]] = None
     validate_shapes: bool = True
 
@@ -72,8 +74,13 @@ class ProteinDocument:
                 self.sequences,
                 self.plddts,
                 self.backbone_coords,
+                self.backbone_coords_masks,
                 self.structure_tokens,
             )
+        if self.backbone_coords_masks is None and self.backbone_coords is not None:
+            self.backbone_coords_masks = [
+                np.ones_like(xyz) for xyz in self.backcbone_coords
+            ]
 
     def __getitem__(self, key):
         if isinstance(key, slice):
@@ -87,6 +94,9 @@ class ProteinDocument:
                 plddts=self.plddts[key] if self.plddts is not None else None,
                 backbone_coords=self.backbone_coords[key]
                 if self.backbone_coords is not None
+                else None,
+                backbone_coords_masks=self.backbone_coords_masks[key]
+                if self.backbone_coords_masks is not None
                 else None,
                 structure_tokens=self.structure_tokens[key]
                 if self.structure_tokens is not None
@@ -108,6 +118,9 @@ class ProteinDocument:
                 backbone_coords=[self.backbone_coords[i] for i in key]
                 if self.backbone_coords is not None
                 else None,
+                backbone_coords_masks=[self.backbone_coords_masks[i] for i in key]
+                if self.backbone_coords_masks is not None
+                else None,
                 structure_tokens=[self.structure_tokens[i] for i in key]
                 if self.structure_tokens is not None
                 else None,
@@ -120,6 +133,9 @@ class ProteinDocument:
                 plddt=self.plddts[key] if self.plddts is not None else None,
                 backbone_coords=self.backbone_coords[key]
                 if self.backbone_coords is not None
+                else None,
+                backbone_coords_mask=self.backbone_coords_masks[key]
+                if self.backbone_coords_masks is not None
                 else None,
                 structure_tokens=self.structure_tokens[key]
                 if self.structure_tokens is not None
@@ -135,12 +151,19 @@ class ProteinDocument:
     def has_all_structure_arrays(self):
         has_arrays = [
             arr is not None
-            for arr in [self.plddts, self.backbone_coords, self.structure_tokens]
+            for arr in [
+                self.plddts,
+                self.backbone_coords,
+                self.backbone_coords_masks,
+                self.structure_tokens,
+            ]
         ]
         missing_arrays_msg = " ".join(
             [
                 f"{name}: {missing}"
-                for name, missing in zip(["plddts", "coords", "tokens"], has_arrays)
+                for name, missing in zip(
+                    ["plddts", "coords", "coords_masks", "tokens"], has_arrays
+                )
             ]
         )
         # print(f"Missing arrays: {missing_arrays_msg}")
@@ -168,6 +191,9 @@ class ProteinDocument:
             positions=kwargs.get("positions", self.positions),
             plddts=kwargs.get("plddts", self.plddts),
             backbone_coords=kwargs.get("backbone_coords", self.backbone_coords),
+            backbone_coords_masks=kwargs.get(
+                "backbone_coords_masks", self.backbone_coords_masks
+            ),
             structure_tokens=kwargs.get("structure_tokens", self.structure_tokens),
             validate_shapes=kwargs.get("validate_shapes", self.validate_shapes),
         )
