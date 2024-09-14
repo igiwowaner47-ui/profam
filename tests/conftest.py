785003cc5bf76e197581aefbd95baba54cf11265
alex-hh <alex.hhooker@gmail.com>
2024-09-14 20:12:33 +0100

    fix foldseek tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,12 +14,9 @@ from omegaconf import DictConfig, open_dict
 
 from src.constants import BASEDIR
 from src.data import preprocessing, transforms
+from src.data.datasets import ProteinDatasetConfig, load_protein_dataset
 from src.data.proteingym import load_gym_dataset
-from src.data.utils import (
-    CustomDataCollator,
-    ProteinDatasetConfig,
-    load_protein_dataset,
-)
+from src.data.utils import CustomDataCollator
 from src.utils.tokenizers import ProFamTokenizer
 
 
