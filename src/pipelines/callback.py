b900c51530aabb81465fb7a80b764097194fa858
alex-hh <alex.hhooker@gmail.com>
2024-09-18 01:11:51 +0100

    try using rank_zero_only decorator to implement esmfold callback

--- src/pipelines/callback.py
+++ src/pipelines/callback.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 from typing import Dict, Optional
 
 from lightning.pytorch.callbacks import Callback
+from lightning.pytorch.utilities.rank_zero import rank_zero_only
 
 from src.evaluators.base import SamplingEvaluator
 from src.models.inference import ProFamSampler, PromptBuilder
@@ -27,6 +28,7 @@ class SamplingEvaluationPipelineCallback(Callback):
         self.prompt_builder = prompt_builder
         self.match_representative_length = match_representative_length
 
+    @rank_zero_only
     def on_validation_epoch_end(self, trainer, model):
         # run on val epoch end rather than train to stay in sync with other validation metrics
         if trainer.sanity_checking:
@@ -39,24 +41,23 @@ class SamplingEvaluationPipelineCallback(Callback):
             sampling_kwargs=self.sampling_kwargs,
             match_representative_length=self.match_representative_length,
         )
-        if trainer.is_global_zero:
-            # https://lightning.ai/docs/pytorch/stable/visualize/logging_advanced.html#rank-zero-only
-            # Q: how does logging work across ranks? if i log only from rank 0, what happens?
-            all_metrics = defaultdict(list)
-            t0 = time.time()
-            # self.pipeline.reset()  # clear stale results (rerun sampler and rerun evaluator should suffice anyway but no harm)
-            results_df = self.pipeline.run(
-                sampler,
-                self.evaluator,
-                verbose=False,
-                rerun_evaluator=True,
-                rerun_sampler=True,
-            )
-            sampler.to(device)
-            mean_results = results_df.mean().to_dict()
-            t1 = time.time()
-            all_metrics = {
-                f"{self.evaluator.name}/{k}": v for k, v in mean_results.items()
-            }
-            all_metrics[f"{self.evaluator.name}/time"] = t1 - t0
-            model.log_dict(all_metrics, on_epoch=True, rank_zero_only=True)
+        # https://lightning.ai/docs/pytorch/stable/visualize/logging_advanced.html#rank-zero-only
+        # Q: how does logging work across ranks? if i log only from rank 0, what happens?
+        all_metrics = defaultdict(list)
+        t0 = time.time()
+        # self.pipeline.reset()  # clear stale results (rerun sampler and rerun evaluator should suffice anyway but no harm)
+        results_df = self.pipeline.run(
+            sampler,
+            self.evaluator,
+            verbose=False,
+            rerun_evaluator=True,
+            rerun_sampler=True,
+        )
+        sampler.to(device)
+        mean_results = results_df.mean().to_dict()
+        t1 = time.time()
+        all_metrics = {
+            f"{self.evaluator.name}/{k}": v for k, v in mean_results.items()
+        }
+        all_metrics[f"{self.evaluator.name}/time"] = t1 - t0
+        model.log_dict(all_metrics, on_epoch=True, rank_zero_only=True)
