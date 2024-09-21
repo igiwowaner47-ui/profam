ab855e60bc730ca4bdfcbedadf48b6b7cc8989b7
alex-hh <alex.hhooker@gmail.com>
2024-09-21 14:55:59 +0100

    log hydra files and commit hash

--- src/utils/loggers.py
+++ src/utils/loggers.py
@@ -1,11 +1,15 @@
+import os
 from argparse import Namespace
 from typing import Any, Dict, Mapping, Optional, Union
 
+import hydra
 from lightning.fabric.loggers.logger import _DummyExperiment as DummyExperiment
 from lightning.pytorch.loggers.logger import Logger
 from lightning.pytorch.loggers.wandb import WandbLogger
 from lightning.pytorch.utilities.rank_zero import rank_zero_only
 from typing_extensions import override
+
+from src.constants import BASEDIR
 
 
 # TODO: use logging
@@ -43,4 +47,29 @@ class StdOutLogger(Logger):
 
 class WandbLogger(WandbLogger):
     # TODO: extended to optionally log hydra config file and git hash
-    pass
+    def __init__(
+        self, log_hydra_config_file: bool = False, log_git_hash: bool = False, **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.log_hydra_config = log_hydra_config_file
+        self.log_git_hash = log_git_hash
+
+    @rank_zero_only
+    def log_hyperparameters(self, hparams, **kwargs):
+        hydra_cfg = hydra.core.hydra_config.HydraConfig.get()
+        hydra_cfg["runtime"]["output_dir"]
+        if self.log_hydra_config_file:
+            import wandb
+
+            artifact = wandb.Artifact("hydra_outputs", type="config")
+            hydra_dir = os.path.join(hydra_cfg["runtime"]["output_dir"], ".hydra")
+            artifact.add_file(os.path.join(hydra_dir, "config.yaml"))
+            artifact.add_file(os.path.join(hydra_dir, "hydra.yaml"))
+            artifact.add_file(os.path.join(hydra_dir, "overrides.yaml"))
+            self.experiment.log_artifact(artifact)
+
+        if self.log_git_hash:
+            with open(os.path.join(BASEDIR, "commit_hash.txt"), "r") as f:
+                commit_hash = f.read().strip()
+            hparams["git_hash"] = commit_hash
+        super().log_hyperparameters(hparams, **kwargs)
