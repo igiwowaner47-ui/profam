2d98cd23bc7bbd330ae292a572f4a4d1786aa389
alex-hh <alex.hhooker@gmail.com>
2024-10-11 12:56:33 +0100

    support sequence packing

--- src/data/processors/batch_transforms.py
+++ src/data/processors/batch_transforms.py
@@ -70,3 +70,59 @@ def pack_batches(
     if examples_to_pack:
         packed_examples.append(pack_examples(examples_to_pack))
     return examples_list_to_dict(packed_examples)
+
+
+# def naive_concatenated_document_batch_sampler(lengths, max_tokens_per_example):
+#     concatenated_length = 0
+#     batch_indices = []
+#     for i, l in enumerate(lengths):
+#         if concatenated_length > max_tokens_per_example:
+#             yield batch_indices
+#             batch_indices = []
+#             concatenated_length = 0
+#         concatenated_length += l
+#         batch_indices.append(i)
+#     yield batch_indices
+
+
+# def concatenate_short_documents(
+#     examples,
+#     batch_sampler,
+#     feature_names: List[str],
+#     max_tokens_per_example: Optional[int] = None,
+# ):
+#     """Concatenate short documents into a single example.
+
+#     This is ultimately a bin-packing problem, if we handle it via a fixed set of examples (i.e. via batched map).
+#     So batch_sampler is a bin-packing sampler.
+#     An approximate bin packing solution:
+#     https://github.com/imoneoi/multipack_sampler/blob/master/multipack_sampler.py
+#     """
+#     # TODO: use logic from DataCollatorWithFlattening
+#     # advantage of doing processing here rather than in collator is that we can
+#     # determine 'batch size' (number of documents) dynamically
+#     additional_features_to_flatten = [
+#         f
+#         for f in feature_names
+#         if f not in ["input_ids", "labels"] and f not in STRING_FEATURE_NAMES
+#     ]
+
+#     flattening_collator = DataCollatorWithFlattening(
+#         separator_id=-100,
+#         additional_features_to_flatten=additional_features_to_flatten,
+#         return_position_ids=False,
+#     )
+#     lengths = [input_ids.shape[0] for input_ids in examples["input_ids"]]
+#     examples_dicts = examples_to_list_of_dicts(examples)
+#     if max_tokens_per_example is None:
+#         batch_indices = [list(range(len(lengths)))]
+#     else:
+#         batch_indices = batch_sampler(lengths, max_tokens_per_example)
+#     concatenated_examples = []
+#     for batch_indices in batch_indices:
+#         batch_examples = [examples_dicts[i] for i in batch_indices]
+#         concatenated_example = flattening_collator.numpy_flatten(batch_examples)
+#         for f in STRING_FEATURE_NAMES:
+#             concatenated_example[f] = "-".join([ex[f] for ex in batch_examples])
+#         concatenated_examples.append(concatenated_example)
+#     return examples_list_to_dict(concatenated_examples)
