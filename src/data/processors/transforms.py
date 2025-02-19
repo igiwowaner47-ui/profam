f4f17623026033b71b34684578dd57fb7bd6e429
judewells <judewells@hotmail.com>
2025-02-19 10:59:46 +0000

    bug fix single seq ted docs clustering

--- src/data/processors/transforms.py
+++ src/data/processors/transforms.py
@@ -117,7 +117,7 @@ def preprocess_raw_sequences_sampling_to_max_tokens(
     Returns:
         A new ProteinDocument containing the sampled proteins.
     """
-    extra_tokens_per_protein = 1 # separator token
+    extra_tokens_per_protein = 1  # separator token
     extra_tokens_per_document = tokenizer.num_start_tokens
 
     rnd = np.random if rng is None else rng
@@ -206,16 +206,16 @@ def preprocess_raw_sequences_sampling_to_max_tokens(
     )
     new_proteins = new_proteins.slice_arrays(array_slices)
     total_tokens = (
-        sum(len(seq) for seq in new_proteins.sequences) + 
-        extra_tokens_per_protein * len(new_proteins) +
-        extra_tokens_per_document
+        sum(len(seq) for seq in new_proteins.sequences)
+        + extra_tokens_per_protein * len(new_proteins)
+        + extra_tokens_per_document
     )
     if total_tokens > max_tokens:
-        bp=1
+        bp = 1
         raise ValueError(
             f"Total tokens {total_tokens} > max_tokens {max_tokens}"
             f"in {proteins.identifier}"
-            )
+        )
     return new_proteins
 
 
@@ -248,7 +248,7 @@ def preprocess_aligned_sequences_sampling_to_max_tokens(
     Returns:
         A new ProteinDocument containing the sampled proteins.
     """
-    extra_tokens_per_protein = 1 # separator token
+    extra_tokens_per_protein = 1  # separator token
     extra_tokens_per_document = tokenizer.num_start_tokens
 
     rnd = np.random if rng is None else rng
