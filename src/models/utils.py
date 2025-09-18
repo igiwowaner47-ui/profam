0a5cafaa8d8e532e18b98d3b553f61e524037cf5
judewells <judewells@hotmail.com>
2025-09-18 10:21:04 +0100

    run script for seeded generations

--- src/models/utils.py
+++ src/models/utils.py
@@ -89,60 +89,6 @@ class InputAwareDynamicCache(DynamicCache):
         return new_cache
 
 
-def DEPRICATED_accuracy_from_outputs(
-    model_outputs,
-    labels,
-    start_ix=0,
-    ignore_index=-100,
-    dataset_names=None,
-    ignore_token_ids: Optional[List[int]] = None,
-    mask=None,
-):
-    # USE metrics.accuracy_from_outputs instead
-    """Compute the accuracy of the target sequence given the model outputs.
-
-    Args:
-        model_outputs: The model outputs from the forward pass.
-        input_ids: The input sequence.
-        ignore_index: Token index to ignore when computing accuracy.
-            (this will get added automatically by the data collator as padding)
-
-    Returns:
-        The accuracy of the target sequence.
-    """
-    labels = labels.clone()
-    if ignore_token_ids is not None:
-        ignore_token_ids = torch.tensor(ignore_token_ids).to(labels.device)
-        labels[torch.isin(labels, ignore_token_ids)] = ignore_index
-    logits = model_outputs.logits
-    # Shift so that tokens < n predict n
-    shift_logits = logits[..., start_ix:-1, :].contiguous()  # b, L, V
-    shift_labels = labels[..., start_ix + 1 :].contiguous()  # b, L
-    if mask is not None:
-        # Shift mask to match the shifted labels
-        shift_mask = mask[..., start_ix + 1 :]
-    # Ensure tensors are on the same device
-    shift_labels = shift_labels.to(shift_logits.device)
-    non_padding_mask = shift_labels != ignore_index
-    if mask is not None:
-        non_padding_mask = non_padding_mask & shift_mask
-    # TODO: we might also want to ignore gaps...
-    accuracy = (shift_logits.argmax(-1) == shift_labels).float()
-    if dataset_names is not None:
-        # N.B. this also works for empty list
-        ds_accuracies = {}
-        for ds_name in set(dataset_names):
-            in_dataset_mask = np.array(dataset_names) == ds_name
-            ds_accuracies[ds_name] = (
-                accuracy[in_dataset_mask] * non_padding_mask[in_dataset_mask]
-            ).sum() / non_padding_mask[in_dataset_mask].sum()
-        ds_accuracies["global"] = (
-            accuracy * non_padding_mask
-        ).sum() / non_padding_mask.sum()
-        return ds_accuracies
-    accuracy = (accuracy * non_padding_mask).sum() / non_padding_mask.sum()
-    return accuracy
-
 
 def log_likelihood_from_outputs(model_outputs, labels, start_ix=0, flatten=False):
     """Compute the negative log likelihood of the target sequence given the model outputs.
